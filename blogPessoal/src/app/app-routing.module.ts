d831cc9dfca04f5706e141f2ef435b85eb3d4fdd
unknown <junior.silva_costa@live.com>
2020-12-05 14:48:23 -0300

    Configurando rotas

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FeedComponent } from './feed/feed.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'}, //adicionando a home como rota padrão
+  {path: 'home', component: HomeComponent}, //adicionando rota para home
+  {path: 'feed', component: FeedComponent}, //adicionando rota para o feed
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- blogPessoal/src/app/perfil-lateral/perfil-lateral.component.ts
+++ blogPessoal/src/app/perfil-lateral/perfil-lateral.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilLateralComponent implements OnInit {
 
+
+  
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    
   }
 
 }
